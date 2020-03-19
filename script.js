e95ef7d5df4b89a385c24e6dbd65d1dc19deaa69
Carson Keller <carsonjkeller@gmail.com>
2020-03-19 17:05:43 -0600

    Corrected JS

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-const name = document.getElementById("name");
+console.log("And your name was?");
 
-const input = localStorage.setItem("theName", "name");
+const submit = document.getElementById("submit")
+const my_input = document.getElementById("input");
+const response = document.getElementById("name")
 
-const theName = localStorage.getItem("theName");
+const btnClicked = (event) => {
+    console.log(event.target.id);
+    response.innerHTML = my_input.value;
+}
+
+submit.addEventListener("click", btnClicked);
 
 
-// const name = document.getElementById("name");
 
-// const btnClicked = (event) => {
-//     theName.innerHTML = name.value
-//     // name.value = "";
-//     localStorage.setItem()
+
+// function gettingTheName (event) {
+//     console.log(event.target.id)
+//     response.innerHTML = my_input.value;
 // }
 
-// name.addEventListener("click", btnClicked);
+
+// my_input.addEventListener("type", gettingTheName);
+
+
+// //  btnClicked = (event) => {
+// //     theName.innerHTML = my_input.value;
+// //     // input.value = "";
+// // }
+
+
+// // localStorage.setItem("input", "name");
+
+// // document.getElementById("input").innerHTML = localStorage.getItem("theName");
+
+
+// // const name = document.getElementById("name");
+
+// // const btnClicked = (event) => {
+// //     theName.innerHTML = name.value
+// //     // name.value = "";
+// //     localStorage.setItem()
+// // }
+
+// submit.addEventListener("click", btnClicked);
