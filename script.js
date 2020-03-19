da8e7143918da0832b3fd96a54598182a3777768
Carson Keller <carsonjkeller@gmail.com>
2020-03-19 14:37:29 -0600

    General improvements (more frequent commits next time, I promise

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log("Name Badge");
+const name = document.getElementById("name");
 
+const btnClicked = (event) => {
+    theName.innerHTML = name.value
+    // name.value = "";
+}
+
+name.addEventListener("click", btnClicked);
