e597acbdd87a7549ebf69eec3fc7a5548ba143b9
Carson Keller <carsonjkeller@gmail.com>
2020-03-20 10:49:23 -0600

    Fixed button functionality & corrected console log record

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("And your name was?");
 
-const submit = document.getElementById("submit")
+const submit = document.getElementById("submit");
 const my_input = document.getElementById("input");
-const response = document.getElementById("name")
+const response = document.getElementById("name");
 
 const btnClicked = (event) => {
     console.log(event.target.id);
@@ -10,36 +10,3 @@ const btnClicked = (event) => {
 }
 
 submit.addEventListener("click", btnClicked);
-
-
-
-
-// function gettingTheName (event) {
-//     console.log(event.target.id)
-//     response.innerHTML = my_input.value;
-// }
-
-
-// my_input.addEventListener("type", gettingTheName);
-
-
-// //  btnClicked = (event) => {
-// //     theName.innerHTML = my_input.value;
-// //     // input.value = "";
-// // }
-
-
-// // localStorage.setItem("input", "name");
-
-// // document.getElementById("input").innerHTML = localStorage.getItem("theName");
-
-
-// // const name = document.getElementById("name");
-
-// // const btnClicked = (event) => {
-// //     theName.innerHTML = name.value
-// //     // name.value = "";
-// //     localStorage.setItem()
-// // }
-
-// submit.addEventListener("click", btnClicked);
